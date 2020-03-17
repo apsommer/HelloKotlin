37f28c4fcfa14c39af2c9cd931e1742b6aa29e50
Drew Sommer <drew@sommerengineering.com>
2020-03-16 20:49:19 -0700

    feat: Explore filters

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    val spices = listOf("curry", "pepper", "cayenne", "ginger", "red curry", "green curry", "red pepper" )
+    val eagerList = spices.filter {it.contains("curry")}.sorted()
+
+    println(eagerList)
+
+    val apples = spices.filter {it.startsWith('c') && it.endsWith('e')}
+    println(apples)
+
+    val bananas = spices.take(3).filter { it.startsWith('c') }
+    println(bananas)
 }
+
+
