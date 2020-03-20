ee1a3ec19e6594f73318aa4021ca98de614704b7
Drew Sommer <drew@sommerengineering.com>
2020-03-19 22:58:52 -0700

    feat: Enums and sealed classes

--- src/spices/Spice.kt
+++ src/spices/Spice.kt
@@ -1,8 +1,8 @@
 package spices
 
-// abstract class is a template, just like Java
+// sealed class is very similar to abstract class, with the extract requirement that all subclasses are contained within this same file
 // "interface delegation": the interface SpiceColor is implemented, and satisfied by the passed color of type SpiceColor
-abstract class Spice(val name : String, spiciness: String, color : SpiceColor) : SpiceColor by color {
+sealed class Spice(val name : String, spiciness: String, color : SpiceColor) : SpiceColor by color {
     abstract fun prepareSpice()
 }
 
@@ -24,14 +24,21 @@ interface Grinder {
 }
 
 interface SpiceColor {
-    val color : String
+    val color : Color
 }
 
 // singleton object of SpiceColor
 object YellowSpiceColor : SpiceColor {
-    override val color: String = "yellow"
+    override val color = Color.YELLOW
 }
 
 data class SimpleContainer(var spice : Spice) {
     val label = spice.name
+}
+
+enum class Color(rbg : Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF),
+    YELLOW(0xFFFF00)
 }
--- src/spices/main.kt
+++ src/spices/main.kt
@@ -5,7 +5,8 @@ fun main(args : Array<String>) {
     val curry: Curry = Curry("curry", "hot")
     print(curry.color)
 
+    println()
+
     val container = SimpleContainer(curry)
     print(container.label)
-
 }
