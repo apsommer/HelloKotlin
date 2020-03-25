aa3a6d783cf96353db092d36ee4261c7cf44a9b9
Drew Sommer <drew@sommerengineering.com>
2020-03-24 18:55:15 -0700

    feat: Collections

--- src/spices/main.kt
+++ src/spices/main.kt
@@ -10,4 +10,16 @@ fun main(args : Array<String>) {
     println("All info: " + book.allInfo())
     val title = book.title
     println("Just the title: $title")
+
+    var allBooks = setOf("Mistborn", "Hero of Ages", "Well of Ascension")
+    var library = mapOf("Sanderson" to allBooks)
+
+    println("Does library include Mistborn? " + library.any {it.value.contains("Mistborn")})
+    println("Does library include Hamlet? " + library.any {it.value.contains("Hamlet")})
+
+    var moreBooks = mutableMapOf("J.R.R. Tolkien" to "Lord of the Rings")
+    moreBooks.getOrPut("Bilbo Baggins") {"The Shire"}
+
+    println("library: $library")
+    println("moreBooks: $moreBooks")
 }
