1370711166416bc3e92edbe624257523463cffb9
Drew Sommer <drew@sommerengineering.com>
2020-03-26 19:13:37 -0700

    docs: Basic lambdas

---
+++ src/directions/Directions.kt
@@ -0,0 +1,34 @@
+package directions
+
+enum class Direction {
+    NORTH, SOUTH, EAST, WEST, START, END
+}
+
+class Game() {
+
+    var path = mutableListOf<Direction>(Direction.START)
+
+    val north = {path.add(Direction.NORTH)}
+    val south = {path.add(Direction.SOUTH)}
+    val east = {path.add(Direction.EAST)}
+    val west = {path.add(Direction.END)}
+
+    val end = {
+        path.add(Direction.END)
+        println("Game over.")
+        println("Path: $path")
+        path.clear()
+        false
+    }
+}
+
+fun main (args : Array<String>) {
+    val game = Game()
+    println(game.path)
+    game.north()
+    game.south()
+    game.east()
+    game.west()
+    game.end()
+    println(game.path)
+}
